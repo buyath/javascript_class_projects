7b0ff16d1dff12ce6a3588b9eab45f75c37ab1f7
Thayub <mthayub@freshworks.com>
2024-10-16 10:12:49 -0500

    instead of buttons counter is created

--- javascript_project_1/index.js
+++ javascript_project_1/index.js
@@ -1,31 +1,33 @@
 var container = document.querySelector(".child-container");
 var head = document.querySelector(".header");
 var link_cont = document.querySelector(".link-container");
+var link = document.querySelector(".links");
+var box = document.querySelector(".boxs");
 var count = 0; 
 var link_count = 0;
 window.onload = () =>{
     alert(head.textContent);
 }
-function link_counter(){
-    alert(link_count);
-}
+
 function adding(){
     var child_cont = document.createElement("div");
     child_cont.setAttribute("class","box-style");
     container.appendChild(child_cont);
-    count  = count + 1;
+    count = count  + 1;
+    box_counter();
 }
-function counter(){
-    alert(count);
-}
+
 function removing(){
     var child_box = document.querySelector(".box-style:last-child");
     if(child_box){
         container.removeChild(child_box);
-        count = count - 1; 
+        count = count - 1;
+        box_counter();
+     
     }
     else{
         alert("there is no box");
+
     }
 }
 
@@ -37,14 +39,23 @@ function create_link(){
     link.setAttribute("class","w3schools");
     link_cont.appendChild(link);
     link_count = link_count + 1;
+    link_counter();
 }
 function remove_link(){
     var link = document.querySelector(".w3schools:last-child");
     if(link){
         link_cont.removeChild(link);
         link_count = link_count - 1;
+        link_counter();
     }
     else{
         alert("There is no link ")
     }
+}
+
+function box_counter(){
+    box.innerHTML = count;
+}
+function link_counter(){
+    link.innerHTML = link_count;
 }
